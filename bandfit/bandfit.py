cc8c18b4e70d613805bbdd336de576f57b23e275
Valentin Boettcher <hiro@protagon.space>
2023-03-18 18:02:52 -0400

    remove failed experiment

--- bandfit/bandfit.py
+++ bandfit/bandfit.py
@@ -15,16 +15,4 @@ def load_data(data_file: str) -> np.ndarray:
     raw -= raw.min()
     raw /= raw.max()
 
-
-    # for i in range(1, raw.shape[0]):
-    #     last_row = raw[i - 1]
-    #     current_row = raw[i]
-
-
-    #     print(np.argmax(signal.correlate(current_row, last_row))-  current_row.size )
-
-    #     raw[i] = np.roll(current_row, current_row.size - np.argmax(np.abs(signal.correlate(current_row, last_row))))
-
     return raw
-
-
